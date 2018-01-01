c575c29185964cb915f9b6fb13d49a26e043c287
Stewart ilondanga <stilondanga@gmail.com>
2018-01-01 21:59:36 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,35 @@ $(".next").click(function(){
 	next_fs.show();
 	//hide the current fieldset with style
 
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			//as the opacity of current_fs reduces to 0 - stored in "now"
+			//1. scale current_fs down to 80%
+			scale = 1 - (1 - now) * 0.2;
+			//2. bring next_fs from the right(50%)
+			left = (now * 50)+"%";
+			//3. increase opacity of next_fs to 1 as it moves in
+			opacity = 1 - now;
+			current_fs.css({
+        'transform': 'scale('+scale+')',
+        'position': 'absolute'
+      });
+			next_fs.css({'left': left, 'opacity': opacity});
+		},
+		duration: 800,
+		complete: function(){
+			current_fs.hide();
+			animating = false;
+		},
+		//this comes from the custom easing plugin
+		easing: 'easeInOutBack'
+	});
+});
+
+$(".previous").click(function(){
+	if(animating) return false;
+	animating = true;
+
 	
 
 var navigate = (function() {
