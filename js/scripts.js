91f5090c676e9bc1789e44347a242547fd5461ee
Stewart ilondanga <stilondanga@gmail.com>
2018-01-01 22:02:02 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -56,7 +56,18 @@ $(".previous").click(function(){
 	previous_fs.show();
 	//hide the current fieldset with style
 
-	
+	current_fs.animate({opacity: 0}, {
+		step: function(now, mx) {
+			//as the opacity of current_fs reduces to 0 - stored in "now"
+			//1. scale previous_fs from 80% to 100%
+			scale = 0.8 + (1 - now) * 0.2;
+			//2. take current_fs to the right(50%) - from 0%
+			left = ((1-now) * 50)+"%";
+			//3. increase opacity of previous_fs to 1 as it moves in
+			opacity = 1 - now;
+			current_fs.css({'left': left});
+
+				
 
 var navigate = (function() {
 	$('.dd').toggle();
