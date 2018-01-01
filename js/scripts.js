79e9d458209ca4920649b2a65e1985a9c18763ef
Stewart ilondanga <stilondanga@gmail.com>
2018-01-01 21:56:46 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
 
+$(".next").click(function(){
+	if(animating) return false;
+	animating = true;
+
 
 
 var navigate = (function() {
