1c7a732c05d6e165227abd1816136e9a3164bc10
Stewart ilondanga <stilondanga@gmail.com>
2018-01-01 21:56:16 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+//jQuery time
+var current_fs, next_fs, previous_fs; //fieldsets
+var left, opacity, scale; //fieldset properties which we will animate
+var animating; //flag to prevent quick multi-click glitches
+
+
+
 var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
